9f33181675f1eadcc35b0799f0c9612e25f8e924
Pham Quoc Cuong <pzcuong@Pham-Quoc-Cuong.local>
2023-03-17 09:39:19 +0700

    Change into gpt-3.5-turbo

--- script.js
+++ script.js
@@ -13,26 +13,24 @@ async function sendChat() {
     event.preventDefault();
   
     let chat = document.getElementById("chat_message").value;
-    
-    let res = await fetch("https://api.openai.com/v1/completions", {
+    UpdateChat("You", chat);
+    document.getElementById("chat_message").value = "";
+
+    let res = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "Authorization": "Bearer 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"
       },
       body: JSON.stringify({
-        "model": "text-davinci-003",
-        "prompt": chat,
-        "max_tokens": 2048,
-        "temperature": 0
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": chat}]
       })
     });  
-    UpdateChat("You", chat);
-  
+
     res = await res.json();
-    let botResponse = res.choices[0].text;
+    let botResponse = res.choices[0].message.content
     UpdateChat("Bot", botResponse);
-    document.getElementById("chat_message").value = "";
 }  
 
 async function UpdateChat(sender, message) {
